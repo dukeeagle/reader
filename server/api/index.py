81c77059f6b0a1d0c35302b19d0b72677622846f
dukeeagle <luke.igel@gmail.com>
2023-11-24 22:24:43 -0600

    update routing

--- server/api/index.py
+++ server/api/index.py
@@ -1,10 +1,23 @@
-from http.server import BaseHTTPRequestHandler
- 
-class handler(BaseHTTPRequestHandler):
- 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def api():
+    return "Hello, world!!!"
+
+@app.route('/narrate', methods=['POST'])
+def narrate():
+    data = request.get_json()
+
+    openAIKey = data.get('openAIKey')
+    elevenLabsKey = data.get('elevenLabsKey')
+    webpageUrl = data.get('webpageUrl')
+
+    # Now you can use the parameters in your function
+    # ...
+
+    return jsonify({'status': 'success'})
+
+if __name__ == '__main__':
+    app.run(port=4200)
